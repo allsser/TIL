6135739694a2099965d7e701c8b3b7a327f67651
allsser <allsser@naver.com>
2019-11-04 17:23:49 +0900

    1104|RESTful & 1N Relation

--- 05_Django/03_django_crud/articles/urls.py
+++ 05_Django/03_django_crud/articles/urls.py
@@ -4,11 +4,17 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
+    # path('',views.index, name="index"), # READ Logic - Index
+    # path('new/',views.new, name='new'), # CREATE Logic - 사용자에게 폼 전달
+    # path('create/',views.create, name='create'), # CREATE Logic - 데이터베이스에 저장
+    # path('<int:article_pk>/', views.detail, name='detail'),  # READ Logic - Detail
+    # path('<int:article_pk>/delete/', views.delete, name='delete'), # DELETE Logic
+    # path('<int:article_pk>/edit/',views.edit, name='edit'),  # UPDATE Logic - 폼 전달
+    # path('<int:article_pk>/update/', views.update, name='update'), # UPDATE Logic - DB 저장
+
     path('',views.index, name="index"), # READ Logic - Index
-    path('new/',views.new, name='new'), # CREATE Logic - 사용자에게 폼 전달
-    path('create/',views.create, name='create'), # CREATE Logic - 데이터베이스에 저장
+    path('create/',views.create, name='create'), # GET(new) / POST(create)
     path('<int:article_pk>/', views.detail, name='detail'),  # READ Logic - Detail
     path('<int:article_pk>/delete/', views.delete, name='delete'), # DELETE Logic
-    path('<int:article_pk>/edit/',views.edit, name='edit'),  # UPDATE Logic - 폼 전달
-    path('<int:article_pk>/update/', views.update, name='update'), # UPDATE Logic - DB 저장
+    path('<int:article_pk>/update/', views.update, name='update'), # FET(edit) / POST(update)
 ]
--- 05_Django/03_django_crud/articles/views.py
+++ 05_Django/03_django_crud/articles/views.py
@@ -8,19 +8,28 @@ def index(request):
   return render(request,'articles/index.html', context)
 
 # 사용자에게 게시글 작성 폼을 보여주는 함수
-def new(request):
-  return render(request, 'articles/new.html')
+# def new(request):
+#   return render(request, 'articles/new.html')
 
 # 사용자로부터 데이터를 받아서 DB에 저장하는 함수
 def create(request):
-  title = request.POST.get('title')
-  content = request.POST.get('content')
+  # POST 요청일 경우 => 게시글 생성 로직 수행
+  if request.method == 'POST':
+    title = request.POST.get('title')
+    content = request.POST.get('content')
 
-  article = Article(title=title, content=content)
-  article.save()
-  # return render(request, 'articles/index.html')
-  return redirect(f'/articles/{ article.pk }')  # 1. 하드코딩
-  # return redirect('articles:detail', article.pk)  # 2. URL Namespace
+    article = Article(title=title, content=content)
+    article.save()
+    # return render(request, 'articles/index.html')
+    return redirect(f'/articles/{ article.pk }')  # 1. 하드코딩
+    # return redirect('articles:detail', article.pk)  # 2. URL Namespace
+    
+  # GET 요청일 경우 => 사용자에게 폼 보여주기
+  else:
+    return render(request, 'articles/create.html')
+
+
+
 
 # 게시글 상세정보를 가져오는 함수
 def detail(request, article_pk):
@@ -34,20 +43,27 @@ def delete(request, article_pk):
   article.delete()
   return redirect('/articles/')
 
-# 사용자한테 게시글 수정 폼을 전달
-def edit(request, article_pk):
-  article = Article.objects.get(pk=article_pk)
-  context = {'article': article}
-  return render(request, 'articles/edit.html',context)
-
 # 수정 내용 전달받아서 DB에 저장(반영)
 def update(request, article_pk):
   article = Article.objects.get(pk=article_pk)
-  # 1. 수정할 게시글 인스턴스 가져오기
-  article.title = request.POST.get('title')
-  # 2. 폼에서 전달받은 데이터 덮어쓰기
-  article.content = request.POST.get('content')
-  # 3. DB 저장
-  article.save()
-  # 4. 저장 끝났으면 게시글 Detail로 이동시키기
-  return redirect(f'/articles/{article.pk}/')
+
+  if request.method == 'POST':
+    # POST 요청 => DB에 수정사항 반영
+    
+    # 1. 수정할 게시글 인스턴스 가져오기
+    article.title = request.POST.get('title')
+    # 2. 폼에서 전달받은 데이터 덮어쓰기
+    article.content = request.POST.get('content')
+    # 3. DB 저장
+    article.save()
+    # 4. 저장 끝났으면 게시글 Detail로 이동시키기
+    return redirect('articles:detail', article.pk)
+
+  # GET 요청 => 사용자에게 수정 Form 전달
+  else:
+    context = {'article': article}
+    return render(request, 'articles/update.html',context)
+
+
+
+
