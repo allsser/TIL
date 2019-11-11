8220e68f9c6e48dde4e72247438c4216684847d8
allsser <allsser@naver.com>
2019-11-11 17:30:50 +0900

    1111|django_form

---
+++ 05_Django/04_django_form/articles/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # Model Level에서 메타데이터 옵션 설정 -> 정렬 기능 사용
+    class Meta:
+        ordering = ('-pk',)
+
+    # 객체 표현 방식
+    def __str__(self):
+        return self.content
